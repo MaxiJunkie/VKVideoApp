ba437b739f48f8f7669d11781ffda11997cfe420
MaxiJunkie <Slavik123>
2017-03-17 18:29:09 +0300

    Initial Commit

---
+++ VKVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VKVideo
+//
+//  Created by Максим Стегниенко on 17.03.17.
+//  Copyright © 2017 Максим Стегниенко. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VKVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKVideo
+//
+//  Created by Максим Стегниенко on 17.03.17.
+//  Copyright © 2017 Максим Стегниенко. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
